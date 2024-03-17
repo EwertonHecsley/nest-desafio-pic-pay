377097f7c57ef57dfec9b37b3611f3b13eb9aaa3
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2024-03-17 20:57:51 -0300

    Criando os metodos para cadastrar usuario, e medotos de buscas

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -13,4 +13,33 @@ export class UserService {
     async listAllUsers(): Promise<CreateUserDto[]> {
         return await this.prisma.user.findMany();
     }
+
+    async getUserByEmail(email: string): Promise<CreateUserDto> {
+        return await this.prisma.user.findUnique({
+            where: {
+                email: email
+            }
+        });
+    }
+
+    async getUserByCPF_CNPJ(cpf_cnpj: string): Promise<CreateUserDto> {
+        return await this.prisma.user.findUnique({
+            where: {
+                cpf_cnpj: cpf_cnpj
+            }
+        });
+    }
+
+    async createUser(dataUser: CreateUserDto): Promise<CreateUserDto> {
+        const { nomeCompleto, cpf_cnpj, email, logista, senha } = dataUser;
+        return await this.prisma.user.create({
+            data: {
+                nomeCompleto: nomeCompleto,
+                cpf_cnpj: cpf_cnpj,
+                email: email,
+                senha: await this.hashService.hashPassword(senha),
+                logista: logista
+            }
+        });
+    }
 }

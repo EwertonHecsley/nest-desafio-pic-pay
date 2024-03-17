bcde367a86728f35c2022f667d6baa7f5eea26bd
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2024-03-16 22:21:06 -0300

    module de users

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { DatabaseModule } from './database/database.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [DatabaseModule, UserModule],
   controllers: [],
   providers: [],
 })

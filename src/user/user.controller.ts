e10ccca50defa1b63c67a7f585319bd862cda705
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2024-03-17 21:05:56 -0300

    endpoint cadastrar usuario

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,27 @@
-import { Controller, Get, Res } from '@nestjs/common';
+import { Body, Controller, Get, HttpException, HttpStatus, Post, Res } from '@nestjs/common';
 import { UserService } from './user.service';
 import { Response } from 'express';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('user')
 export class UserController {
     constructor(private readonly userService: UserService) { }
+
+    @Post()
+    async create(@Body() user: CreateUserDto, @Res() res: Response) {
+
+        const verifyEmail = await this.userService.getUserByEmail(user.email);
+
+        if (verifyEmail) throw new HttpException('Email já cadastrado.', HttpStatus.BAD_GATEWAY);
+
+        const vrifyCPF_CNPJ = await this.userService.getUserByCPF_CNPJ(user.cpf_cnpj);
+
+        if (vrifyCPF_CNPJ) throw new HttpException('CPF/CNPJ já cadastrado.', HttpStatus.BAD_GATEWAY);
+
+        const result = await this.userService.createUser(user);
+
+        return res.status(HttpStatus.CREATED).json(result);
+    }
 
     @Get()
     async listAllUsers(@Res() res: Response) {

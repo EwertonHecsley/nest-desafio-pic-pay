24b948c022080d5132730dec30d6fb0acc1ad2b9
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2024-03-17 22:40:31 -0300

    modulo de transacoes

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { UserModule } from './user/user.module';
 import { HashServiceService } from './hash-service/hash-service.service';
+import { TransacaoModule } from './transacao/transacao.module';
 
 @Module({
-  imports: [DatabaseModule, UserModule],
+  imports: [DatabaseModule, UserModule, TransacaoModule],
   controllers: [],
   providers: [HashServiceService],
 })

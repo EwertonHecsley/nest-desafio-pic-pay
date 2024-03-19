5bcfaab86b6f0f6ede5e15e835d4a6c14e5f41e4
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2024-03-19 20:05:15 -0300

    module auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { DatabaseModule } from './database/database.module';
 import { UserModule } from './user/user.module';
 import { HashServiceService } from './hash-service/hash-service.service';
 import { TransacaoModule } from './transacao/transacao.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [DatabaseModule, UserModule, TransacaoModule],
+  imports: [DatabaseModule, UserModule, TransacaoModule, AuthModule],
   controllers: [],
   providers: [HashServiceService],
 })
